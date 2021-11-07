16480f4e70907427daa57d3b760ca08fa33556bc
Mauno Erhardt <maunoerhardt@web.de>
2021-11-07 11:18:13 +0100

    Split check routines for IOS and NXOS in separate functions
    Reason: improve performance and address pylint issue "R0915: Too many statements (65/50)"

--- check_cisco_envtemp.py
+++ check_cisco_envtemp.py
@@ -117,6 +117,161 @@ def get_snmp_table(table_oid, args):
     return table
 
 
+def check_ios_device(args):
+    """ check Cisco IOS device """
+    # Cisco IOS switch, using CISCO-ENVMON-MIB
+
+    # Get temperature values
+    # (CISCO-ENVMON-MIB::ciscoEnvMonTemperatureStatusValue)
+    temp_values = get_snmp_table('1.3.6.1.4.1.9.9.13.1.3.1.3', args)
+
+    # Get vendor defined thresholds
+    # (CISCO-ENVMON-MIB::ciscoEnvMonTemperatureThreshold)
+    temp_thresholds = get_snmp_table('1.3.6.1.4.1.9.9.13.1.3.1.4', args)
+
+    # Get temperature state (CISCO-ENVMON-MIB::ciscoEnvMonTemperatureState)
+    temp_state = get_snmp_table('1.3.6.1.4.1.9.9.13.1.3.1.6', args)
+
+    # Remove everything except identifier from SNMP OID
+    # ('SNMPv2-SMI::enterprises.9.9.13.1.3.1.3.1008 ' -> '1008')
+    # And strip blanks from OID values (' 42' -> '42')
+    for entry in chain(temp_values, temp_thresholds, temp_state):
+        entry[0] = entry[0].strip().split(".")[-1:]
+        entry[0] = "".join(map(str, entry[0]))
+        entry[1] = entry[1].strip()
+
+    if len(temp_values) == 0 or len(temp_thresholds) == 0 or \
+       len(temp_state) == 0:
+        # Check if we received data via SNMP, otherwise exit with state
+        # Unknown
+        exit_plugin("3", "No data returned via SNMP", "NULL")
+
+    # Create perfdata and output strings
+    perfdata = ""
+    output = "Sensor readings are: "
+    for i, _ in enumerate(temp_values):
+        # loop through sensors and construct return and perfdata string
+
+        if args.scale is not None:
+            # Do not apply MIB-definded thresholds,
+            # instead scale them by <threshold_scale>%
+            temp_thresholds[i][1] = round(float(temp_thresholds[i][1]) *
+                                          (args.scale / 100), 1)
+
+        perfdata += "\'temp_" + str(temp_values[i][0]) + "\'=" + \
+                    str(temp_values[i][1]) + ";;" + \
+                    str(temp_thresholds[i][1]) + ";; "
+        output += str(temp_values[i][1]) + "°C, "
+
+    # Remove last comma from output string
+    output = output.rstrip(', ')
+
+    # Calculate return code
+    returncode = "0"
+    for i, _ in enumerate(temp_values):
+        if float(temp_values[i][1]) >= float(temp_thresholds[i][1]) and \
+           float(temp_thresholds[i][1]) != 0:
+            returncode = "2"
+
+    exit_plugin(returncode, output, perfdata)
+
+
+def check_nxos_device(args):
+    """ check Cisco IOS device """
+    # Cisco NX-OS switch, using CISCO-ENTITY-SENSOR-MIB
+
+    # Get sensor type (CISCO-ENTITY-SENSOR-MIB::entSensorType)
+    sensor_type = get_snmp_table('1.3.6.1.4.1.9.9.91.1.1.1.1.1', args)
+
+    # Get sensor type (CISCO-ENTITY-SENSOR-MIB::entSensorValue)
+    sensor_values = get_snmp_table('1.3.6.1.4.1.9.9.91.1.1.1.1.4', args)
+
+    # Get sensor thresholds
+    # (CISCO-ENTITY-SENSOR-MIB::entSensorThresholdValue)
+    sensor_thresholds = get_snmp_table('1.3.6.1.4.1.9.9.91.1.2.1.1.4', args)
+
+    # Get sensor scale (CISCO-ENTITY-SENSOR-MIB::entSensorScale)
+    sensor_scale = get_snmp_table('1.3.6.1.4.1.9.9.91.1.1.1.1.2', args)
+
+    # Format returned SNMP data to lists
+    for entry in chain(sensor_type, sensor_values, sensor_scale):
+        entry[0] = entry[0].strip().split(".")[-1:]
+        entry[0] = "".join(map(str, entry[0]))
+        entry[1] = entry[1].strip()
+
+    for entry in sensor_thresholds:
+        entry[0] = entry[0].strip().split(".")[-2:]
+        entry[1] = entry[1].strip()
+
+    if len(sensor_type) == 0 or len(sensor_values) == 0 or \
+       len(sensor_thresholds) == 0 or len(sensor_scale) == 0:
+        # Check if we received data via SNMP, otherwise exit with state
+        # Unknown
+        exit_plugin("3", "No data returned via SNMP", "NULL")
+
+    # Create list with identifiers of temperature sensors from
+    # CISCO-ENTITY-SENSOR-MIB::entSensorType
+    tempsensorids = []
+    for i in sensor_type:
+        if str(i[1]) == "8":
+            tempsensorids.append(i[0])
+
+    # Set return code and generate output and perfdata strings
+    returncode = "0"
+    perfdata = ""
+    output = "Sensor readings are: "
+
+    for i in tempsensorids:
+        # loop through temperature sensors
+        sensor = i
+
+        for entry in sensor_values:
+            # Get value for sensor ID
+            if str(entry[0]) == str(sensor):
+                val = float(entry[1])
+
+        for entry in sensor_thresholds:
+            # Get warn and crit thresholds for sensor ID
+            if str(entry[0][0]) == str(sensor):
+                if str(entry[0][1]) == "1":
+                    warn = float(entry[1])
+                if str(entry[0][1]) == "2":
+                    crit = float(entry[1])
+
+        for entry in sensor_scale:
+            # Get scaling factor for sensor value
+            if str(entry[0]) == str(sensor):
+                scale = str(entry[1])
+
+        if str(scale) == "8":
+            # Scaling factor is milli(8), divide val, warn and crin /1000
+            val = round(val / 1000, 2)
+            warn = warn / 1000
+            crit = crit / 1000
+
+        if args.scale is not None:
+            # Do not apply MIB-definded thresholds,
+            # instead scale them by <threshold_scale>%
+            warn = round(warn * (args.scale / 100), 1)
+            crit = round(crit * (args.scale / 100), 1)
+
+        # Append to perfdata and output string
+        perfdata += ''.join(["\'temp_", str(sensor), "\'=", str(val), ";",
+                             str(warn), ";", str(crit), ";; "])
+        output += ''.join([str(val), "°C, "])
+
+        # Calculate return code
+        if val >= crit > 0:
+            returncode = "2"
+        elif (val >= warn > 0) and (returncode != "2"):
+            returncode = "1"
+
+    # Remove last comma from output string
+    output = output.rstrip(', ')
+
+    exit_plugin(returncode, output, perfdata)
+
+
 def exit_plugin(returncode, output, perfdata):
     """ Check status and exit accordingly """
     if returncode == "3":
@@ -140,155 +295,10 @@ def main():
     args = get_args()
 
     if args.os == "ios":
-        # Cisco IOS switch, using CISCO-ENVMON-MIB
-
-        # Get temperature values
-        # (CISCO-ENVMON-MIB::ciscoEnvMonTemperatureStatusValue)
-        temp_values = get_snmp_table('1.3.6.1.4.1.9.9.13.1.3.1.3', args)
-
-        # Get vendor defined thresholds
-        # (CISCO-ENVMON-MIB::ciscoEnvMonTemperatureThreshold)
-        temp_thresholds = get_snmp_table('1.3.6.1.4.1.9.9.13.1.3.1.4', args)
-
-        # Get temperature state (CISCO-ENVMON-MIB::ciscoEnvMonTemperatureState)
-        temp_state = get_snmp_table('1.3.6.1.4.1.9.9.13.1.3.1.6', args)
-
-        # Remove everything except identifier from SNMP OID
-        # ('SNMPv2-SMI::enterprises.9.9.13.1.3.1.3.1008 ' -> '1008')
-        # And strip blanks from OID values (' 42' -> '42')
-        for entry in chain(temp_values, temp_thresholds, temp_state):
-            entry[0] = entry[0].strip().split(".")[-1:]
-            entry[0] = "".join(map(str, entry[0]))
-            entry[1] = entry[1].strip()
-
-        if len(temp_values) == 0 or len(temp_thresholds) == 0 or \
-           len(temp_state) == 0:
-            # Check if we received data via SNMP, otherwise exit with state
-            # Unknown
-            exit_plugin("3", "No data returned via SNMP", "NULL")
-
-        # Create perfdata and output strings
-        perfdata = ""
-        output = "Sensor readings are: "
-        for i, val in enumerate(temp_values):
-            # loop through sensors and construct return and perfdata string
-
-            if args.scale is not None:
-                # Do not apply MIB-definded thresholds,
-                # instead scale them by <threshold_scale>%
-                temp_thresholds[i][1] = round(float(temp_thresholds[i][1]) *
-                                              (args.scale / 100), 1)
-
-            perfdata += "\'temp_" + str(temp_values[i][0]) + "\'=" + \
-                        str(temp_values[i][1]) + ";;" + \
-                        str(temp_thresholds[i][1]) + ";; "
-            output += str(temp_values[i][1]) + "°C, "
-
-        # Remove last comma from output string
-        output = output.rstrip(', ')
-
-        # Calculate return code
-        returncode = "0"
-        for i, val in enumerate(temp_values):
-            if float(temp_values[i][1]) >= float(temp_thresholds[i][1]) and \
-               float(temp_thresholds[i][1]) != 0:
-                returncode = "2"
-
-        exit_plugin(returncode, output, perfdata)
+        check_ios_device(args)
 
     if args.os == "nxos":
-        # Cisco NX-OS switch, using CISCO-ENTITY-SENSOR-MIB
-
-        # Get sensor type (CISCO-ENTITY-SENSOR-MIB::entSensorType)
-        sensor_type = get_snmp_table('1.3.6.1.4.1.9.9.91.1.1.1.1.1', args)
-
-        # Get sensor type (CISCO-ENTITY-SENSOR-MIB::entSensorValue)
-        sensor_values = get_snmp_table('1.3.6.1.4.1.9.9.91.1.1.1.1.4', args)
-
-        # Get sensor thresholds
-        # (CISCO-ENTITY-SENSOR-MIB::entSensorThresholdValue)
-        sensor_thresholds = get_snmp_table('1.3.6.1.4.1.9.9.91.1.2.1.1.4', args)
-
-        # Get sensor scale (CISCO-ENTITY-SENSOR-MIB::entSensorScale)
-        sensor_scale = get_snmp_table('1.3.6.1.4.1.9.9.91.1.1.1.1.2', args)
-
-        # Format returned SNMP data to lists
-        for entry in chain(sensor_type, sensor_values, sensor_scale):
-            entry[0] = entry[0].strip().split(".")[-1:]
-            entry[0] = "".join(map(str, entry[0]))
-            entry[1] = entry[1].strip()
-
-        for entry in sensor_thresholds:
-            entry[0] = entry[0].strip().split(".")[-2:]
-            entry[1] = entry[1].strip()
-
-        if len(sensor_type) == 0 or len(sensor_values) == 0 or \
-           len(sensor_thresholds) == 0 or len(sensor_scale) == 0:
-            # Check if we received data via SNMP, otherwise exit with state
-            # Unknown
-            exit_plugin("3", "No data returned via SNMP", "NULL")
-
-        # Create list with identifiers of temperature sensors from
-        # CISCO-ENTITY-SENSOR-MIB::entSensorType
-        tempsensorids = []
-        for i in sensor_type:
-            if str(i[1]) == "8":
-                tempsensorids.append(i[0])
-
-        # Set return code and generate output and perfdata strings
-        returncode = "0"
-        perfdata = ""
-        output = "Sensor readings are: "
-
-        for i in tempsensorids:
-            # loop through temperature sensors
-            sensor = i
-
-            for entry in sensor_values:
-                # Get value for sensor ID
-                if str(entry[0]) == str(sensor):
-                    val = float(entry[1])
-
-            for entry in sensor_thresholds:
-                # Get warn and crit thresholds for sensor ID
-                if str(entry[0][0]) == str(sensor):
-                    if str(entry[0][1]) == "1":
-                        warn = float(entry[1])
-                    if str(entry[0][1]) == "2":
-                        crit = float(entry[1])
-
-            for entry in sensor_scale:
-                # Get scaling factor for sensor value
-                if str(entry[0]) == str(sensor):
-                    scale = str(entry[1])
-
-            if str(scale) == "8":
-                # Scaling factor is milli(8), divide val, warn and crin /1000
-                val = round(val / 1000, 2)
-                warn = warn / 1000
-                crit = crit / 1000
-
-            if args.scale is not None:
-                # Do not apply MIB-definded thresholds,
-                # instead scale them by <threshold_scale>%
-                warn = round(warn * (args.scale / 100), 1)
-                crit = round(crit * (args.scale / 100), 1)
-
-            # Append to perfdata and output string
-            perfdata += ''.join(["\'temp_", str(sensor), "\'=", str(val), ";",
-                                 str(warn), ";", str(crit), ";; "])
-            output += ''.join([str(val), "°C, "])
-
-            # Calculate return code
-            if val >= crit > 0:
-                returncode = "2"
-            elif (val >= warn > 0) and (returncode != "2"):
-                returncode = "1"
-
-        # Remove last comma from output string
-        output = output.rstrip(', ')
-
-        exit_plugin(returncode, output, perfdata)
+        check_nxos_device(args)
 
 
 if __name__ == "__main__":
